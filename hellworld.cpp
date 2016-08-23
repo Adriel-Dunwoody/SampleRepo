3785f7b03156d22c856f42d58c5cdf7c9511377f
Adriel Dunwoody <dunwoody@usc.edu>
2016-08-23 15:25:10 -0700

    Added 3 more lines

--- hellworld.cpp
+++ hellworld.cpp
@@ -4,5 +4,8 @@ int main(int argc, char* argv[])
 {
 	std::cout << "Hello World" << std::endl;
 	std::cout << "Get ready to for a world of pain!" << std::endl;
+	std::cout << "BLAH!" << std::endl;
+	std::cout << "BLAH!" << std::endl;
+
 	return 0;
 }
