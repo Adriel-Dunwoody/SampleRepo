574f1d3c4bce68e62ca7c1502debdc3da48fbc7e
Adriel Dunwoody <dunwoody@usc.edu>
2016-08-23 15:20:46 -0700

    fixed new cout line

--- hellworld.cpp
+++ hellworld.cpp
@@ -3,6 +3,6 @@
 int main(int argc, char* argv[])
 {
 	std::cout << "Hello World" << std::endl;
-	std::cout << "Get ready to for a world of pain!"
+	std::cout << "Get ready to for a world of pain!" << std::endl;
 	return 0;
 }
